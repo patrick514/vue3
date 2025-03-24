7030b3700634b068ff8eeaa199fab4165fb7d045
patrick514 <2528726609@qq.com>
2025-03-24 16:16:29 +0800

    build

---
+++ packages/reactivity/dist/reactivity.js
@@ -0,0 +1,8 @@
+// packages/shared/src/index.ts
+function isObject(value) {
+  return typeof value === "object" && value !== null;
+}
+
+// packages/reactivity/src/index.ts
+console.log(isObject("isbo"));
+//# sourceMappingURL=reactivity.js.map
--- scripts/dev.js
+++ scripts/dev.js
@@ -1,8 +1,41 @@
 // 这个文件会帮我们打包 packages下的模块， 最终打包出js文件
 
-
 import minimist from "minimist";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+import { createRequire } from "module";
+import esbuild from "esbuild";
+
 
 const args = minimist(process.argv.slice(2))
 
-console.log(args)
+// esm 使用commonjs 变量
+const __filename = fileURLToPath(import.meta.url); // 获取文件的绝对路径 file: -> /usr
+const __dirname = dirname(__filename);
+const require = createRequire(import.meta.url);
+const target = args._[0] || "reactivity"; // 打包哪个项目
+const format = args.f || "iife"; // 打包后的模块化规范
+
+// 入口文件 根据命令行提供的路径来进行解析
+const entry = resolve(__dirname, `../packages/${target}/src/index.ts`);
+const pkg = require(`../packages/${target}/package.json`);
+// 根据需要进行打包
+
+console.log(resolve(__dirname, `../packages/${target}/dist/${target}.js`));
+//C:\Users\lyq\OneDrive\Desktop\study\vue3\packages\reactivity\dist\reactivity.js
+esbuild
+  .context({
+    entryPoints: [entry], // 入口
+    outfile: resolve(__dirname, `../packages/${target}/dist/${target}.js`), // 出口
+    bundle: true, // reactivity -> shared  会打包到一起
+    platform: "browser", // 打包后给浏览器使用
+    sourcemap: true, // 可以调试源代码
+    format, // cjs esm iife
+    globalName: pkg.buildOptions?.name,
+  })
+  .then((ctx) => {
+    console.log("start dev");
+    return ctx.watch(); // 监控入口文件持续进行打包处理
+  });
+// console.log(args)
+//{ _: [ 'reactivity' ], f: 'esm' }
